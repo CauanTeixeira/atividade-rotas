f9578e9d515b086159727687a9139406fc6fa95b
CauanTeixeira <cauan.jesus.ct@outlook.com>
2023-06-18 22:35:01 -0300

    migrations

---
+++ rotas/database/migrations/2023_06_19_013238_create_vendas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('quantidade');
+            $table->string('valor');
+            $table->string('produto_id');
+            $table->string('produto_nome');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendas');
+    }
+};
